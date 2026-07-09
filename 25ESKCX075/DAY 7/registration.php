fe202a9d7701f571335c9dccfa904cd159ba8f1d
pari0204 <guptapari202@gmail.com>
2026-07-09 08:31:08 +0530

    Create registration.php for user registration handling
    This file handles user registration by collecting personal information from a POST request and echoing the received values.

---
+++ 25ESKCX075/DAY 7/registration.php
@@ -0,0 +1,16 @@
+?php
+
+$fullname = $_POST['fullname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$birthdate = $_POST['birthdate'];
+$gender = $_POST['gender'];
+$address1 = $_POST['address1'];
+$address2 = $_POST['address2'];
+$country = $_POST['country'];
+$city = $_POST['city'];
+$region = $_POST['region'];
+$postalcode = $_POST['postalcode'];
+
+echo "value received : $fullname $email $phone $birthdate $gender $address1 $address2 $country $city $region $postalcode";
+?>
