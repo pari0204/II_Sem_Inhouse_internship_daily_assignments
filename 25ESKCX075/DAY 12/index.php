db31e32a8d1ad85fcf939a00fbb5d77ac59ce371
pari0204 <guptapari202@gmail.com>
2026-07-12 23:33:35 +0530

    Create index.php

---
+++ 25ESKCX075/DAY 12/index.php
@@ -0,0 +1,42 @@
+<?php
+include("header.php");
+?>
+
+<div class="container mt-5">
+
+<div class="row align-items-center">
+
+<div class="col-md-6">
+
+<h1 class="display-4">
+Student Management Portal
+</h1>
+
+<p class="lead">
+
+This project is developed using
+
+PHP + MySQL + Bootstrap.
+
+</p>
+
+<a href="register.php" class="btn btn-primary btn-lg">
+Get Started
+</a>
+
+</div>
+
+<div class="col-md-6 text-center">
+
+<img src="https://cdn-icons-png.flaticon.com/512/3135/3135755.png"
+width="300">
+
+</div>
+
+</div>
+
+</div>
+
+<?php
+include("footer.php");
+?>
