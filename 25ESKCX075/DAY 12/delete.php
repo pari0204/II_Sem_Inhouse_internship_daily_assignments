f2cf883e99efc1cf994866ee96e1f0ca399fb47f
pari0204 <guptapari202@gmail.com>
2026-07-12 23:24:34 +0530

    Add delete.php for user deletion functionality

---
+++ 25ESKCX075/DAY 12/delete.php
@@ -0,0 +1,11 @@
+<?php
+include("session.php");
+include("db_connect.php");
+
+$id = $_GET['id'];
+
+mysqli_query($conn,"DELETE FROM users WHERE id='$id'");
+
+header("Location: dashboard.php");
+exit();
+?>
