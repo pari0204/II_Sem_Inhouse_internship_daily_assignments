e6704b92e5597c56e9c87ba9c7bcf4930f74820e
pari0204 <guptapari202@gmail.com>
2026-07-12 23:13:12 +0530

    Create header.php

---
+++ 25ESKCX075/DAY 11/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Student Login System</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
