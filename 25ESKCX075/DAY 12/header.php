e17eaa6235062d85fb3d7592ef443bc2c5d16bf7
pari0204 <guptapari202@gmail.com>
2026-07-12 23:32:14 +0530

    Add header.php for student portal layout

---
+++ 25ESKCX075/DAY 12/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Student Portal</title>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+
+<div class="container">
+
+<a class="navbar-brand" href="index.php">
+Student Portal
+</a>
+
+<div>
+
+<a href="register.php" class="btn btn-light me-2">
+Register
+</a>
+
+<a href="login.php" class="btn btn-warning">
+Login
+</a>
+
+</div>
+
+</div>
+
+</nav>
